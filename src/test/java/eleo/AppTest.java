e7a0e4896cb094511146fadc3e4e2bb0fe45e70a
Eleoper <123415922+Eleoper@users.noreply.github.com>
2024-09-23 21:00:31 -0500

    deleted 2 cases for test

--- src/test/java/eleo/AppTest.java
+++ src/test/java/eleo/AppTest.java
@@ -13,28 +13,6 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void 2times2()
-    {
-        int num1 = 2;
-        int num2 = 2;
-        int total = num1 * num2;
-        if(total == 4)
-        {
-            assertTrue(true);
-        }
-    }
-
-    public void oneplusone()
-    {
-        int num1 = 1;
-        int num2 = 2;
-        int total = num1 + num2;
-        if(total == 2)
-        {
-            assertTrue(true);
-        }
-    }
-
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
