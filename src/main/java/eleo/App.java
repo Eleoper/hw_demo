ba040c3873ff1a75346ae968495ef1723e514c79
EleoPer <emapute@test.one>
2024-09-23 19:39:24 -0500

    first commit

---
+++ src/main/java/eleo/App.java
@@ -0,0 +1,29 @@
+package eleo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(add3toNum(3));
+        System.out.println(concatString("Hello ", "World"));
+        
+    }
+
+    public static int add3toNum(int num){
+        int total = num + 3;
+        return total;
+    }
+    
+
+    public static String concatString(String firstStr, String secondStr){
+        String result = "";
+
+        result = firstStr + secondStr;
+
+        return result;
+    }
+}
