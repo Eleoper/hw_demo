296a57560bc6f322e0ba94caff1e5bf91df805f3
Eleoper <123415922+Eleoper@users.noreply.github.com>
2024-09-23 21:05:29 -0500

    2 test cases added again

--- src/test/java/eleo/AppTest.java
+++ src/test/java/eleo/AppTest.java
@@ -13,14 +13,28 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void 2times2()
     {
-        assertTrue( true );
+        int num1 = 2;
+        int num2 = 2;
+        int total = num1 * num2;
+        if(total == 4)
+        {
+            assertTrue(true);
+        }
     }
-    public void shouldAnswerWithTrue()
+    @Test
+    public void oneplusone()
     {
-        assertTrue( true );
+        int num1 = 1;
+        int num2 = 2;
+        int total = num1 + num2;
+        if(total == 2)
+        {
+            assertTrue(true);
+        }
     }
+    @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
